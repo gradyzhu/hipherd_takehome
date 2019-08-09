026091ffcb16be5c7af762949d011485b371e19b
your name here <gradyzhoo@gmail.com>
2019-08-09 16:38:49 -0700

    Update Bundler for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_08_06_053932) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "complete", null: false
